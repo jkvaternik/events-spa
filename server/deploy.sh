3d00651cfdf449b52497d18950d865f0808f3c0b
Jaime <jaime.kvaternik17@gmail.com>
2021-03-26 09:07:15 -0400

    Add deployment stuff

---
+++ server/deploy.sh
@@ -0,0 +1,40 @@
+export MIX_ENV=prod
+# Common port range for this is 4000-10,000
+# Valid port range for a user app to listen
+# on is something like 1025-32767
+export PORT=4969
+export SECRET_KEY_BASE=insecure
+export DATABASE_URL=ecto://events:bad@localhost/events_spa_prod
+
+mix deps.get --only prod
+mix compile
+
+CFGD=$(readlink -f ~/.config/events_spa)
+
+if [ ! -d "$CFGD" ]; then
+    mkdir -p "$CFGD"
+fi
+
+if [ ! -e "$CFGD/base" ]; then
+    mix phx.gen.secret > "$CFGD/base"
+fi
+
+if [ ! -e "$CFGD/db_pass" ]; then
+    pwgen 12 1 > "$CFGD/db_pass"
+fi
+
+SECRET_KEY_BASE=$(cat "$CFGD/base")
+export SECRET_KEY_BASE
+
+DB_PASS=$(cat "$CFGD/db_pass")
+export DATABASE_URL=ecto://events:$DB_PASS@localhost/events_spa_prod
+
+mix ecto.reset
+mix ecto.create
+mix ecto.migrate
+
+npm install --prefix ./assets
+npm run deploy --prefix ./assets
+mix phx.digest
+
+mix release
---
+++ server/start.sh
@@ -0,0 +1,17 @@
+export MIX_ENV=prod
+export PORT=4969
+
+CFGD=$(readlink -f ~/.config/events_board)
+
+if [ ! -e "$CFGD/base" ]; then
+    echo "run deploy first"
+    exit 1
+fi
+
+DB_PASS=$(cat "$CFGD/db_pass")
+export DATABASE_URL=ecto://events:$DB_PASS@localhost/events_spa_prod
+
+SECRET_KEY_BASE=$(cat "$CFGD/base")
+export SECRET_KEY_BASE
+
+_build/prod/rel/events_spa/bin/events_spa start
